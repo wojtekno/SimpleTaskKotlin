9ebd97ad668ef64fce484bea17dc175720f10af3
wojtek <wjw.nowak@gmail.com>
2020-08-10 11:18:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simpletask_kotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.simpletask_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simpletask_kotlin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
